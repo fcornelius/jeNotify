81d738a605f7e7a20888f8bb34f31530ba3d8c42
Felix Cornelius <mail@felixcornelius.de>
2016-08-01 17:14:07 +0200

    Update config template for ids and sql credentials

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$login = array('j_username' => '',      // s-nr.
+               'j_password' => '');     // pw
+$pin   = array('pin'        => '');     // matr-nr.
+
+$db    = array('host'  => '',            // sqldb credentials
+               'user'  => '',
+               'pw'    => ''),
+               'e_tab' => 'exams';      // sql table for exams list
+
+ ?>
