a66498aecf53ba5d5c8c4bfe7c4d0791756cc1c6
Felix Cornelius <mail@felixcornelius.de>
2016-08-02 20:22:28 +0200

    updated config template

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+$login = array('j_username' => '',
+               'j_password' => '');
+$pin   = array('pin'        => '');
+
+$mail_header = array('from'     => '',
+                     'reply-to' => '');
+// DB credentials
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+define('EX_TABLE','');             // The table used for storing exam names
+define('CL_TABLE','');    // The table used for client email adresses
+define('LOG_TABLE', '');   // Table to store Time logs
+
+ ?>
